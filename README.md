d3986fcbe198c2c5031604895d46a3e5838f871a
Kristian Quirapas <kmquirapas@gmail.com>
2022-07-09 14:14:03 +0800

    Added README and icons

---
+++ README.md
@@ -0,0 +1,25 @@
+<div align="center">
+	<img width="150px" src="icons/large.png" alt="Paper Logo">
+	<h2>Vouchers</h2>
+	<p>
+		<strong>Solana Vouchers</strong>
+	</p>
+	<p>
+		<img src="https://img.shields.io/github/issues/kquirapas/solana-vouchers?style=for-the-badge" alt="Issues">
+		<img src="https://img.shields.io/twitter/follow/k_quirapas?color=%2300acee&style=for-the-badge" alt="Twitter">
+		<img src="https://img.shields.io/github/commit-activity/w/kquirapas/solana-vouchers?style=for-the-badge" alt="Commit Activity">
+		<img src="https://img.shields.io/github/license/kquirapas/solana-vouchers?style=for-the-badge" alt="License">
+	</p>
+</div>
+
+
+## Contribution
+
+Thank you for your interest in contributing to Paper! Please see [CONTRIBUTING.md](./CONTRIBUTING.md) to learn how.
+
+## Much love to these people
+<div align="left">
+<a href="https://github.com/kquirapas/solana-vouchers/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=kquirapas/solana-vouchers" />
+</a>
+</div>
--- programs/vouchers/src/lib.rs
+++ programs/vouchers/src/lib.rs
@@ -2,14 +2,53 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+
+// For specifying the different functions / transactions the program can do
 #[program]
 pub mod vouchers {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn store(ctx: Context<Store>, code: String) -> Result<()> {
+        // CPI over here
+
+        Ok(())
+    }
+
+    pub fn redeem(ctx: Context<Redeem>) -> Result<()> {
+        // CPI over here
         Ok(())
     }
 }
 
+// For specifying the accounts involved in every transaction
+// Note: Space is only computed for accounts
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Store<'info> {
+    // Initialize the account with CPI
+    // Specify the payer of the account creation
+    // 8 byte space for discriminator + Space for account
+    // 1 byte space for holder.bump
+    #[account(init, payer = authority, space = 8 + 1)]
+    pub holder: Account<'info, Holder>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Redeem<'info> {
+    #[account(mut)]
+    pub holder: Account<'info, Holder>,
+    pub authority: Signer<'info>
+}
+
+// Data Structures / Layout of data in memory
+#[account]
+pub struct Holder {
+    bump: u8
+}
+
+// Custom Errors
+pub enum PaperWalletError {
+    InvalidInstructionCommand
+}
